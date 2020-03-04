3c43230524924acd457c804c6e1a93dfb8d3208f
GSA-Eric-M <eric.matyko@gsa.gov>
2020-03-04 08:02:25 -0800

    updates from Eric E thoughts

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -13,26 +13,25 @@ import { Location } from '@reach/router'
 const Sidenav = () => (
   <aside className="usa-layout-docs-sidenav desktop:grid-col-2">
     <nav>
-    <ul className="usa-sidenav">
-      <h3>Related Topics:</h3>  
+    <ul className="usa-sidenav"> 
       <Location>
       {({ location }) => {
         console.log(location)
         if (location.pathname.startsWith('/whycloud/')) {
-          return (<div><li className="usa-sidenav__item"><Link to='/whycloud/overview' activeClassName="usa-focus">Cloud Basics</Link></li>
+          return (<div><h3>Related Topics:</h3><li className="usa-sidenav__item"><Link to='/whycloud/overview' activeClassName="usa-focus">Cloud Basics</Link></li>
           <li className="usa-sidenav__item"><Link to='/whycloud/security' activeClassName="usa-focus">Cloud Security</Link></li>
           <li className="usa-sidenav__item"><Link to='/whycloud/technicalcapabilities' activeClassName="usa-focus">Cloud Capabilities</Link></li></div>)
         } 
         else if (location.pathname.startsWith('/cloudplanning/')) {
 
-          return (<div><li className="usa-sidenav__item"><Link to='/cloudplanning/overview' activeClassName="usa-focus">Planning Basics</Link></li>
+          return (<div><h3>Related Topics:</h3><li className="usa-sidenav__item"><Link to='/cloudplanning/overview' activeClassName="usa-focus">Planning Basics</Link></li>
           <li className="usa-sidenav__item"><Link to='/cloudplanning/business' activeClassName="usa-focus">Business Considerations</Link></li>
           <li className="usa-sidenav__item"><Link to='/cloudplanning/technical' activeClassName="usa-focus">Technical Implementation</Link></li></div>)
 
         }
         else if (location.pathname.startsWith('/findcloud/')) {
 
-          return (<div><li className="usa-sidenav__item"><Link to='/findcloud/iaas' activeClassName="usa-focus">IaaS</Link></li>
+          return (<div><h3>Related Topics:</h3><li className="usa-sidenav__item"><Link to='/findcloud/iaas' activeClassName="usa-focus">IaaS</Link></li>
           <li className="usa-sidenav__item"><Link to='/findcloud/paas' activeClassName="usa-focus">PaaS</Link></li>
           <li className="usa-sidenav__item"><Link to='/findcloud/saas' activeClassName="usa-focus">SaaS</Link></li>
           <li className="usa-sidenav__item"><Link to='/findcloud/professionalservices' activeClassName="usa-focus">CPS</Link></li>
@@ -41,7 +40,7 @@ const Sidenav = () => (
         }
         else if (location.pathname.startsWith('/acquisitionguidance/')) {
 
-          return (<div><li className="usa-sidenav__item"><Link to='/acquisitionguidance/overview' activeClassName="usa-focus">Acquisition Overview</Link></li>
+          return (<div><h3>Related Topics:</h3><li className="usa-sidenav__item"><Link to='/acquisitionguidance/overview' activeClassName="usa-focus">Acquisition Overview</Link></li>
           <li className="usa-sidenav__item"><Link to='/acquisitionguidance/pricing' activeClassName="usa-focus">Pricing</Link></li>
           <li className="usa-sidenav__item"><Link to='/acquisitionguidance/marketresearch' activeClassName="usa-focus">Market Research</Link></li>
           <li className="usa-sidenav__item"><Link to='/acquisitionguidance/procurementprocess' activeClassName="usa-focus">Procurement Process</Link></li>
@@ -50,6 +49,22 @@ const Sidenav = () => (
           <li className="usa-sidenav__item"><Link to='/acquisitionguidance/templates' activeClassName="usa-focus">Templates</Link></li>
           <li className="usa-sidenav__item"><Link to='/acquisitionguidance/usecases' activeClassName="usa-focus">Use Cases</Link></li>
           <li className="usa-sidenav__item"><Link to='/acquisitionguidance/helpfuldocuments' activeClassName="usa-focus">Helpful Documents</Link></li>
+          <li className="usa-sidenav__item"><a href="#TOP" activeClassName="usa-focus"><strong>Top of Page</strong></a></li>
+          </div>)
+        }
+        else if (location.pathname.startsWith('/policy')) {
+
+          return (<div><h3>On This Page:</h3> <li className="usa-sidenav__item"><a href="#FITARA" activeClassName="usa-focus">FITARA</a></li>
+          <li className="usa-sidenav__item"><a href="#MEGABYTE" activeClassName="usa-focus">MEGABYTE</a></li>
+          <li className="usa-sidenav__item"><a href="#CLOUDFIRST" activeClassName="usa-focus">CLOUD FIRST</a></li>
+          <li className="usa-sidenav__item"><a href="#CLOUDSMART" activeClassName="usa-focus">CLOUD SMART</a></li>
+          <li className="usa-sidenav__item"><a href="#DCOI" activeClassName="usa-focus">DCOI</a></li>
+          <li className="usa-sidenav__item"><a href="#FedRAMP" activeClassName="usa-focus">FedRAMP</a></li>
+          <li className="usa-sidenav__item"><a href="#ITMODERNIZATION" activeClassName="usa-focus">IT MODERNIZATION</a></li>
+          <li className="usa-sidenav__item"><a href="#CENTEROFEXCELLENCE" activeClassName="usa-focus">CENTER OF EXCELLENCE</a></li>
+          <li className="usa-sidenav__item"><a href="#CLOUDADOPTION" activeClassName="usa-focus">CLOUD ADOPTION</a></li>
+          <li className="usa-sidenav__item"><a href="#USSM" activeClassName="usa-focus">USSM</a></li>
+          <li className="usa-sidenav__item"><a href="#OMBGUIDANCE" activeClassName="usa-focus">OMB GUIDANCE</a></li>
           </div>)
         }
       }}
--- src/md-pages/findcloud/vendors.md
+++ src/md-pages/findcloud/vendors.md
@@ -6,4 +6,8 @@ sidenav: true
 
 ## FedRAMP Compliant Vendors
 
-Visit the [FedRAMP marketplace]("https://marketplace.fedramp.gov") for FedRAMP compliant vendors.
+Visit the [FedRAMP marketplace](https://marketplace.fedramp.gov) for FedRAMP compliant vendors.
+
+The Federal Risk and Authorization Management Program (FedRAMP) is a government-wide program that provides a standardized approach to security assessment, authorization, and continuous monitoring for cloud products and services.
+
+FedRAMP enables Agencies to rapidly adapt from old, insecure legacy IT to mission-enabling, secure, and cost effective cloud-based IT.
