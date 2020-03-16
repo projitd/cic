58a46ec67b934800aeb41e97dcfcbbc3eab2b787
Sri Deevi <sdeevi@sevatec.com>
2020-03-16 10:20:29 -0400

    adding side bar and fixing the styling

--- src/pages/whycloud/csplist.js
+++ src/pages/whycloud/csplist.js
@@ -2,6 +2,7 @@ import Layout from '../../components/layout';
 import React, { useEffect, useState } from 'react';
 import { getPageData } from '../../../.cache/socketIo';
 import { graphql, Link } from 'gatsby';
+import { Location } from '@reach/router';
 
 const CspList = () => {
   // const endpoint = 'https://github.com/18F/fedramp-data/blob/master/data/data.json';
@@ -23,40 +24,47 @@ const CspList = () => {
   return (
     <Layout>
       <h1>Cloud Service Provider List</h1>
-      <div className="usa-layout-docs usa-section">
-        <div className="grid-container">
-          <div className="grid-row grid-gap">
-            <div className="usa-layout-docs__main desktop:grid-col-12 usa-prose">
-              <div className="grid-row grid-gap">
-                {results.length > 0 ? (
-                  <ol>
-                    {results.map((r, idx) => (
-                      <li
-                        key={idx}
-                        className="padding-bottom-5 margin-top-4 usa-prose border-bottom-05 border-base-lightest"
-                      >
-                        <div className='eachProvider'><img className='providerImg' src={r.CSP_URL} alt="Img Here" /></div> - {r.Cloud_Service_Provider_Name}
-                      </li>
-                    ))}
-                  </ol>
-                ) : (
-                  <h4 className="title">
-                    No Cloud Service Providers Available
-                  </h4>
-                )}
-              </div>
-            </div>
+      <div className="grid-container">
+        <div className="grid-row">
+          <div className="desktop:grid-col-10 usa-prose padding-right-4">
+            {results.length > 0 ? (
+              <ol>
+                {results.map((r, idx) => (
+                  <li
+                    key={idx}
+                    className="padding-bottom-5 margin-top-4 usa-prose border-bottom-05 border-base-lightest"
+                  >
+                    <div className='eachProvider'><img className='providerImg' src={r.CSP_URL} alt="Img Here"/></div>
+                    - {r.Cloud_Service_Provider_Name}
+                  </li>
+                ))}
+              </ol>
+            ) : (
+              <h4 className="title">
+                No Cloud Service Providers Available
+              </h4>
+            )}
           </div>
+          <aside className="desktop:grid-col-2 margin-top-4 padding-right-4">
+            <nav>
+              <ul className="usa-sidenav">
+                <div><h3>Related Topics:</h3>
+                  <li className="usa-sidenav__item"><Link to='/whycloud/overview' activeClassName="usa-focus">Cloud
+                    Basics</Link></li>
+                  <li className="usa-sidenav__item"><Link to='/whycloud/security' activeClassName="usa-focus">Cloud
+                    Security</Link></li>
+                  <li className="usa-sidenav__item"><Link to='/whycloud/technicalcapabilities' activeClassName="usa-focus">Cloud Capabilities</Link></li>
+                  <li className="usa-sidenav__item"><Link to='/whycloud/csplist' activeClassName="usa-focus">Cloud Service Provider List</Link></li>
+                </div>
+              </ul>
+            </nav>
+
+          </aside>
         </div>
       </div>
     </Layout>
   );
 };
-
-/*<div className="usa-layout-docs__main desktop:grid-col-5 eachProvider" key={item.id}>
-  <img className='providerImg' src={item.Logo} alt="Img Here" />
-  <p>{item.Name}</p>
-</div>*/
 
 
 export default CspList;
