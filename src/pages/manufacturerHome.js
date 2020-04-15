c972c2066bbca3c8d6ad107ee95e0e9f2825cee7
srirdeevi <4fe402948d8013b1a8bea38952a639f5f0e0260f>
2020-04-15 13:16:04 -0400

    adding pmo home page

--- src/pages/manufacturerHome.js
+++ src/pages/manufacturerHome.js
@@ -11,8 +11,37 @@ export default class ManufacturerHome extends React.Component {
     render() {
         return (
             <Layout>
-                <h1>Manufacturer</h1>
+                <div className="grid-container margin-top-105 margin-bottom-105">
+                    <legend className="usa-legend">Manufacturer Information</legend>
+                    <div className="grid-row">
+                        <div className="grid-col-12">
+                            <label className="usa-label" htmlFor="csp_name">Manufacturer Name</label>
+                            <input className="usa-input" id="csp_name" name="csp_name" type="text"/>
+                        </div>
+                    </div>
+                    <div className="grid-row">
+                        <div className="grid-col-6">
+                            <label className="usa-label" htmlFor="csp_website">Manufacturer Website</label>
+                            <input className="usa-input" id="csp_website" name="csp_website" type="text"/>
+                        </div>
+                        <div className="grid-col-6">
+                            <label className="usa-label" htmlFor="duns_number">DUNS Number</label>
+                            <input className="usa-input" id="duns_number" name="duns_number" type="text"/>
+                        </div>
+                    </div>
+                    <div className="grid-row">
+                        <div className="grid-col-6">
+                            <label className="usa-label" htmlFor="poc_name">POC Name</label>
+                            <input className="usa-input" id="poc_name" name="poc_name" type="text"/>
+                        </div>
+                        <div className="grid-col-6">
+                            <label className="usa-label" htmlFor="poc_email">POC Email</label>
+                            <input className="usa-input" id="poc_email" name="poc_email" type="text"/>
+                        </div>
+                    </div>
+                </div>
             </Layout>
         )
     }
 }
+
--- src/pages/pmoHome.js
+++ src/pages/pmoHome.js
@@ -11,45 +11,56 @@ export default class PmoHome extends React.Component {
     render() {
         return (
             <Layout>
-                <h1>this is test</h1>
-                <table className="usa-table usa-table--borderless">
-                    <caption>Borderless table: A borderless table can be useful when you want the information to feel
-                        more a part of the text it accompanies and extends.
-                    </caption>
-                    <thead>
-                    <tr>
-                        <th scope="col">Document Title</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Year</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <th scope="row">Declaration of Independence</th>
-                        <td>Statement adopted by the Continental Congress declaring independence from the British
-                            Empire.
-                        </td>
-                        <td>1776</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Bill of Rights</th>
-                        <td>The first ten amendments of the U.S. Constitution guaranteeing rights and freedoms.</td>
-                        <td>1791</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Declaration of Sentiments</th>
-                        <td>A document written during the Seneca Falls Convention outlining the rights that American
-                            women should be entitled to as citizens.
-                        </td>
-                        <td>1848</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">Emancipation Proclamation</th>
-                        <td>An executive order granting freedom to slaves in designated southern states.</td>
-                        <td>1863</td>
-                    </tr>
-                    </tbody>
-                </table>
+                <div className="grid-container">
+                    <table className="usa-table">
+                        <caption>CSP List</caption>
+                        <thead>
+                        <tr>
+                            <th scope="col">CSP</th>
+                            <th scope="col">CSP Details</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Declaration of Independence</th>
+                            <td>Statement adopted by the Continental Congress declaring independence from the British
+                                Empire.
+                            </td>
+                            <td>
+                                <button className="usa-button">Edit</button>
+                                <button className="usa-button usa-button--secondary">Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Bill of Rights</th>
+                            <td>The first ten amendments of the U.S. Constitution guaranteeing rights and freedoms.</td>
+                            <td>
+                                <button className="usa-button">Edit</button>
+                                <button className="usa-button usa-button--secondary">Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Declaration of Sentiments</th>
+                            <td>A document written during the Seneca Falls Convention outlining the rights that American
+                                women should be entitled to as citizens.
+                            </td>
+                            <td>
+                                <button className="usa-button">Edit</button>
+                                <button className="usa-button usa-button--secondary">Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Emancipation Proclamation</th>
+                            <td>An executive order granting freedom to slaves in designated southern states.</td>
+                            <td>
+                                <button className="usa-button">Edit</button>
+                                <button className="usa-button usa-button--secondary">Delete</button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
             </Layout>
         )
     }
